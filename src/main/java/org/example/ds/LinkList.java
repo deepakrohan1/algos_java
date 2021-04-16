c85d0c19f672eff6534880b8a66ef1d3cbd0bef1
deepakrohan <deepakrohan@hotmail.com>
2021-04-15 21:43:26 -0400

    palindrome

--- src/main/java/org/example/ds/LinkList.java
+++ src/main/java/org/example/ds/LinkList.java
@@ -1,4 +1,64 @@
 package org.example.ds;
 
 public class LinkList {
+
+    Nod head;
+
+    public Nod reverseLinkList() {
+        Nod prev = null;
+        Nod current = this.head;
+
+        while(current != null) {
+            Nod next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+
+//        this.head = current;
+        return prev;
+    }
+
+    public Nod addData(int data) {
+        Nod newNod = new Nod(data);
+        if (this.head == null) {
+            this.head = newNod;
+            return this.head;
+        }
+        Nod current = this.head;
+        while (current != null && current.next != null) {
+            current = current.next;
+        }
+        current.next = newNod;
+        return current;
+    }
+
+    public static void main(String[] args) {
+        LinkList li = new LinkList();
+        li.addData(10);
+        li.addData(11);
+        li.addData(12);
+        li.addData(13);
+        System.out.println(li.head);
+        System.out.println(li.reverseLinkList());
+    }
+
+}
+
+class Nod {
+    Nod next;
+    int data;
+
+    Nod(int data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "Nod{" +
+
+                "data=" + data +
+                ", next=" + next +
+                '}';
+    }
 }
--- src/main/java/org/example/problems/Palindrome.java
+++ src/main/java/org/example/problems/Palindrome.java
@@ -1,22 +1,42 @@
 package org.example.problems;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class Palindrome {
 
-    public boolean isAnagram(String a , List<String> arr) {
+    public List<String> isAnagram( List<String> arr) {
 
-        
+        List<String> anagrams = new ArrayList<>();
+        for(String word: arr) {
+            boolean isA = true;
 
-        return false;
+            for (int i = 0; i < word.length(); i ++) {
+                int endSeq = word.length() - 1 - i;
+                if (word.charAt(i) != word.charAt(endSeq) && i != endSeq) {
+                    isA = false;
+                    break;
+                }
+            }
+
+            if (isA) {
+                anagrams.add(word);
+            }
+
+        }
+
+
+
+        return anagrams;
     }
 
     public static void main(String[] args) {
-        String s = "AABBBCCC";
-        String[] elements = new String[]{"TTT", "AAAAA"};
+        String[] elements = new String[]{"radar", "civic", "AABAA", "Penguin"};
         List<String> seqList = Arrays.asList(elements);
+        Palindrome p = new Palindrome();
+        System.out.println(p.isAnagram(seqList));
 
 
     }
